e1ce63c918b5f4dee3cbe50ccecf98276677bfd6
AbhinRustagi <rustagi.abhin@gmail.com>
2025-02-22 23:22:22 +0530

    feat: Add list display & filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
+"use client";
+
+import { IListItem, ListItem, tags } from "@/components";
+import data from "@/data/list.json";
+import { useMemo, useState } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const all = data as IListItem[];
+  const [tag, setTag] = useState<keyof typeof tags | "All">("All");
+  const [items, setItems] = useState<IListItem[]>(all);
+
+  useMemo(() => {
+    if (tag === "All") {
+      setItems(all);
+      return;
+    }
+    // Filter the list based on the selected tag
+    setItems(all.filter((item) => item.tag === tag));
+  }, [all, tag]);
+
+  return (
+    <div>
+      <div className="flex gap-4">
+        <div
+          className="border border-neutral-300 hover:bg-neutral-200/60 p-1 px-3 rounded-2xl text-sm flex items-center cursor-pointer gap-2"
+          onClick={() => setTag("All")}
+        >
+          <div className={`rounded-full relative w-2 h-2 bg-indigo-400`}></div>{" "}
+          All
+        </div>
+        {Object.entries(tags).map(([display, color]) => (
+          <div
+            key={`tag-${display}`}
+            className="border border-neutral-300 hover:bg-neutral-200/60 p-1 px-3 rounded-2xl text-sm flex items-center cursor-pointer gap-2"
+            onClick={() => setTag(display as keyof typeof tags)}
+          >
+            <div className={`rounded-full relative w-2 h-2 ${color}`}></div>{" "}
+            {display}
+          </div>
+        ))}
+      </div>
+      <ul className="px-3 my-8">
+        {items.map((item, index) => (
+          <ListItem key={`item-${index + 1}`} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/components.tsx
+++ src/components.tsx
@@ -1,5 +1,17 @@
 import Link from "next/link";
 
+export interface IListItem {
+  title: string;
+  url: string;
+  tag: keyof typeof tags;
+}
+
+export const tags = {
+  Article: "bg-red-600",
+  Video: "bg-teal-500",
+  Other: "bg-violet-500",
+};
+
 export function Header() {
   return (
     <header className="mb-12">
@@ -8,12 +20,42 @@ export function Header() {
       </h1>
       <p>
         A project by{" "}
-        <Link href="https://www.abhin.dev/" className="underline text-blue-500">
+        <Link
+          href="https://www.abhin.dev/"
+          className="underline text-green-600"
+        >
           Abhin Rustagi
         </Link>
         . This is a collection of articles & resources I bookmarked that I find
-        insightful, fascinating, or just plain useful.
+        insightful, fascinating, or just plain useful. Inspired by{" "}
+        <Link
+          href="https://www.fuckiwishiknewth.at/"
+          className="underline text-blue-500"
+          target="_blank"
+          rel="noopener"
+        >
+          F*ck I wish I knew that
+        </Link>
+        .
       </p>
     </header>
+  );
+}
+
+export function ListItem(props: IListItem) {
+  const color = tags[props.tag];
+
+  return (
+    <li className="mb-3">
+      <Link
+        href={props.url}
+        className="flex items-center gap-2 hover:underline"
+        target="_blank"
+        rel="noopener"
+      >
+        <div className={`rounded-full relative w-2 h-2 mr-2 ${color}`}></div>
+        {props.title}
+      </Link>
+    </li>
   );
 }
